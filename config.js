2ef39905b3affbbbbbeb4e8526662869df7a6323
maixuanhh <miniupi04@gmail.com>
2025-06-12 12:11:55 +0700

    config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    introTitle: 'Em bé của chị ơi!!!',
+    introDesc: `Em bé ơi nay chị rảnh rang
+    Muốn nắm tay bé dạo ngang phố phường
+    Cà phê - bánh ngọt yêu thương
+    Hẹn hò một chút cho đời đáng êuuuuu`,
+    btnIntro: 'Hhhhhhhh <3',
+    title: 'Phải chăng chị đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng chị đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Cũng cũngggg á <33',
+    btnNo: 'Sếnnnnnnn :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại va vào chị dọ <3',
+    btnReply: 'Gửi cho em nò <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'Chị biết moà 🥰. Em bé cute nhắm nhắm 😘😘',
+    messDesc: 'Chị sẵn sàng rồi đó, e chọn thời gian đi, chị qua đón nạ.',
+    btnAccept: 'Em đồng ý ạ <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }

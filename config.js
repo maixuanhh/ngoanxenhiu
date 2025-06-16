c8f2a6bc354266982487e0dfa422dee6d8fe464b
maixuanhh <miniupi04@gmail.com>
2025-06-16 18:34:59 +0700

    config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
     introTitle: 'Em bé của chị ơi!!!',
-    introDesc: `Em bé ơi nay chị rảnh rang
-    Muốn nắm tay bé dạo ngang phố phường
-    Cà phê - bánh ngọt yêu thương
+    introDesc: `Em bé ơi nay chị rảnh rang,
+    Muốn nắm tay bé dạo ngang phố phường,
+    Cà phê - bánh ngọt yêu thương,
     Hẹn hò một chút cho đời đáng êuuuuu`,
-    btnIntro: 'Hhhhhhhh <3',
+    btnIntro: 'Aweeee <3',
     title: 'Phải chăng chị đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng chị đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Cũng cũngggg á <33',
@@ -14,7 +14,7 @@ const CONFIG = {
     btnReply: 'Gửi cho em nò <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Chị biết moà 🥰. Em bé cute nhắm nhắm 😘😘',
-    messDesc: 'Chị sẵn sàng rồi đó, e chọn thời gian đi, chị qua đón nạ.',
+    messDesc: 'Chị sẵn sàng cho thứ 4 rồi nạ. Nhớ emmmmmm!!! 2 em 1 cún nhá ^^',
     btnAccept: 'Em đồng ý ạ <3',
     messLink: 'https://maixuanhh.github.io/lovingu/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
